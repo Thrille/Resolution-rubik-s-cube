5cbfeb5dea666592b7961102006ad7db9a9dc96e
Thrille <mandrelierv@yahoo.com>
2020-03-23 09:08:10 +0100

    initialisation code

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Resolution-rubik-s-cube
+
+Ce programme est fait dans le cadre d'un projet d'école afin de permettre la génération d'algorithme de résolution de rubik's cube avec une caméra.
+
+## Installation
+
+Cloner le repos avec :
+
+```bash
+git clone https://github.com/Thrille/Resolution-rubik-s-cube.git
+```
+
+il faut les librairie numpy et opencv:
+
+```bash
+pip3 install numpy
+```
+
+```
+pip3 install opencv
+```
+
+## Utilisation
+
+Pour lancer le programme il faut utiliser:
+
+```
+python3 main.py
+```
+
---
+++ main.py
@@ -0,0 +1,135 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+video = cv2.VideoCapture(0)
+
+cv2.namedWindow("Frame")
+cv2.createTrackbar("L-H", "Frame", 0, 180, nothing)
+cv2.createTrackbar("L-S", "Frame", 100, 255, nothing)
+cv2.createTrackbar("L-V", "Frame", 100, 255, nothing)
+cv2.createTrackbar("U-H", "Frame", 180, 180, nothing)
+cv2.createTrackbar("U-S", "Frame", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Frame", 255, 255, nothing)
+
+font = cv2.FONT_HERSHEY_COMPLEX
+
+
+#ok ~
+lower_red = np.array([151,100,100])
+upper_red = np.array([180,255,255])
+
+#ok
+lower_blue = np.array([101,100,100])
+upper_blue = np.array([150,255,255])
+
+#ok
+lower_green = np.array([46,100,100])
+upper_green = np.array([100,255,255])
+
+#ok
+lower_orange = np.array([0,100,100])
+upper_orange = np.array([15,255,255])
+
+#ok
+lower_yellow = np.array([16,100,100])
+upper_yellow = np.array([60,255,255])
+
+lower_white = np.array([0,0,0])
+upper_white = np.array([0,0,255])
+
+while True:
+    _, frame = video.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("L-H", "Frame")
+    l_s = cv2.getTrackbarPos("L-S", "Frame")
+    l_v = cv2.getTrackbarPos("L-V", "Frame")
+    u_h = cv2.getTrackbarPos("U-H", "Frame")
+    u_s = cv2.getTrackbarPos("U-S", "Frame")
+    u_v = cv2.getTrackbarPos("U-V", "Frame")
+    #lower_red = np.array([l_h,l_s,l_v])
+    #upper_red = np.array([u_h,u_s,u_v])
+
+    mask_red = cv2.inRange(hsv, lower_red, upper_red)
+    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask_green = cv2.inRange(hsv, lower_green, upper_green)
+    mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
+    mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    mask_white = cv2.inRange(hsv, lower_white, upper_white)
+    kernel = np.ones((7,7), np.uint8)
+    mask_red = cv2.erode(mask_red, kernel)
+    mask_blue = cv2.erode(mask_blue, kernel)
+    mask_green = cv2.erode(mask_green, kernel)
+    mask_orange = cv2.erode(mask_orange, kernel)
+    mask_yellow = cv2.erode(mask_yellow, kernel)
+    mask_white = cv2.erode(mask_white, kernel)
+    #contour
+    contours_red,_ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_blue,_ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_green,_ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_orange,_ = cv2.findContours(mask_orange, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_yellow,_ = cv2.findContours(mask_yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_white,_ = cv2.findContours(mask_white, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt_red in contours_red:
+        area = cv2.contourArea(cnt_red)
+       
+        if area > 250:
+            x,y,w,h = cv2.boundingRect(cnt_red)
+            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+            cv2.putText(frame, "rouge", (x,y), font, 1, (255,255,255))
+
+    for cnt_blue in contours_blue:
+        area = cv2.contourArea(cnt_blue)
+
+        if area > 250:
+            x,y,w,h = cv2.boundingRect(cnt_blue)
+            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.putText(frame, "bleu", (x,y), font, 1, (255,255,255))
+
+    for cnt_green in contours_green:
+        area = cv2.contourArea(cnt_green)
+    
+        if area > 250:
+            x,y,w,h = cv2.boundingRect(cnt_green)
+            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(frame, "vert", (x,y), font, 1, (255,255,255))
+
+    for cnt_orange in contours_orange:
+        area = cv2.contourArea(cnt_orange)
+
+        if area > 250:
+            x,y,w,h = cv2.boundingRect(cnt_orange)
+            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),2)
+            cv2.putText(frame, "orange", (x,y), font, 1, (255,255,255))
+
+    for cnt_yellow in contours_yellow:
+        area = cv2.contourArea(cnt_yellow)
+
+        if area > 250:
+            x,y,w,h = cv2.boundingRect(cnt_yellow)
+            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),2)
+            cv2.putText(frame, "yellow", (x,y), font, 1, (255,255,255))
+
+    for cnt_white in contours_white:
+        area = cv2.contourArea(cnt_white)
+
+        if area > 250:
+            x,y,w,h = cv2.boundingRect(cnt_white)
+            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),2)
+            cv2.putText(frame, "white", (x,y), font, 1, (255,255,255))
+
+
+    cv2.imshow("video", frame)
+    cv2.imshow("mask", mask_red)
+    cv2.imshow("mask_blue", mask_blue)
+    key=cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
